5ac04a9da20647bd22c305ea50e379595aca9465
TungLam111 <38311934+TungLam111@users.noreply.github.com>
2021-04-30 14:08:56 +0700

    improved score

--- node_classification/main.py
+++ node_classification/main.py
@@ -39,11 +39,11 @@ def process_data():
         for j in range(k, nums_of_samples):
             calculate_cosine = cosine_similarity2(df_tensor[k], df_tensor[j])
             data_matrix[k][j] = calculate_cosine
-            if calculate_cosine >= 0.9997 :
+            if calculate_cosine >= 0.99995:
                 list_edges.append({"prob": calculate_cosine, "source": k ,"destination": j })
                 list_src.append(k)
                 list_des.append(j)
-
+    print(data_matrix)
     return df_tensor, df_node - 1, df_x, df_y, list_edges , list_src, list_des
 
 def cosine_similarity2(target_vec, vec):
@@ -88,7 +88,8 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
         self.conv1 = GraphConv(in_feats, hidden_size)
         self.conv2 = GraphConv(hidden_size, hidden_size)
-        self.conv3 = GraphConv(hidden_size, num_classes)
+        self.conv3 = GraphConv(hidden_size, 9)
+        self.conv4 = GraphConv(hidden_size, num_classes)
 
     def forward(self, g, inputs):
         h = self.conv1(g, inputs)
@@ -96,6 +97,8 @@ class GCN(nn.Module):
         h = self.conv2(g, h)
         h = torch.relu(h)
         h = self.conv3(g, h)
+        h = torch.relu(h)
+        h = self.conv4(g, h)
         return h
 
 df_tensor, df_node, df_x, df_y, list_edges , list_src, list_des = process_data()
@@ -103,7 +106,7 @@ G = build_graph(list_src, list_des)
 print('We have %d nodes.' % G.number_of_nodes())
 print('We have %d edges.' % G.number_of_edges())
 G.ndata['feat'] = df_tensor
-    
+
 nx_G = G.to_networkx().to_undirected()
 
 pos = nx.kamada_kawai_layout(nx_G)
@@ -112,14 +115,14 @@ nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
 df_tensor = df_tensor.to(dtype=torch.float32)
 
 inputs = df_tensor
-labeled_nodes = torch.tensor([0,15,20,25,40,45,60,    70,85,90,105,110,125,140])
-labels = torch.tensor([0,0,0,0,0,0,0,   1,1,1,1,1,1,1])  # their labels are different
+labeled_nodes = torch.tensor([0,15,20,25,40,45,60,    70,85,90,100,105,110,125,140])
+labels = torch.tensor([0,0,0,0,0,0,0,   1,1,1,1,1,1,1,1])  # their labels are different
 
-net = GCN(9, 16, nums_of_labels)
+net = GCN(9, 9, nums_of_labels)
 embed = nn.Embedding.from_pretrained(df_tensor)
-optimizer = torch.optim.Adam(itertools.chain(net.parameters(), embed.parameters()), lr=0.001)
+optimizer = torch.optim.Adam(itertools.chain(net.parameters(), embed.parameters()), lr=0.005)
 all_logits = []
-for epoch in range(50):
+for epoch in range(100):
     logits = net(G, inputs)
     # we save the logits for visualization later
     all_logits.append(logits.detach())
@@ -136,68 +139,73 @@ for epoch in range(50):
 fig = plt.figure(dpi=150)
 fig.clf()
 ax = fig.subplots()
-draw(49)  # draw the prediction of the first epoch
+draw(99)  # draw the prediction of the first epoch
 plt.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """
-1
-1
-0
-1
-0
-0
-0
-1
-0
-0
-1
-1
-1
-1
-0
-0
-1
-1
-0
-1
-0
-1
-0
-0
-0
-1
-0
-1
-0
-0
-0
-0
-0
-0
+
+def build_karate_club_graph():
+    # All 78 edges are stored in two numpy arrays. One for source endpoints
+    # while the other for destination endpoints.
+    src = np.array([1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 10, 10,
+        10, 11, 12, 12, 13, 13, 13, 13, 16, 16, 17, 17, 19, 19, 21, 21,
+        25, 25, 27, 27, 27, 28, 29, 29, 30, 30, 31, 31, 31, 31, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 33,
+        33, 33, 33, 33, 33, 33, 33, 33, 33, 33])
+    dst = np.array([0, 0, 1, 0, 1, 2, 0, 0, 0, 4, 5, 0, 1, 2, 3, 0, 2, 2, 0, 4,
+        5, 0, 0, 3, 0, 1, 2, 3, 5, 6, 0, 1, 0, 1, 0, 1, 23, 24, 2, 23,
+        24, 2, 23, 26, 1, 8, 0, 24, 25, 28, 2, 8, 14, 15, 18, 20, 22, 23,
+        29, 30, 31, 8, 9, 13, 14, 15, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30,
+        31, 32])
+    # Edges are directional in DGL; Make them bi-directional.
+    u = np.concatenate([src, dst])
+    v = np.concatenate([dst, src])
+    # Construct a DGLGraph
+    return dgl.graph((u, v))
+
+G = build_karate_club_graph()
+print('We have %d nodes.' % G.number_of_nodes())
+print('We have %d edges.' % G.number_of_edges())
+
+nx_G = G.to_networkx().to_undirected()
+# Kamada-Kawaii layout usually looks pretty for arbitrary graphs
+pos = nx.kamada_kawai_layout(nx_G)
+nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
+
+embed = nn.Embedding(34, 5)  # 34 nodes with embedding dim equal to 5
+G.ndata['feat'] = embed.weight
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, hidden_size, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GraphConv(in_feats, hidden_size)
+        self.conv2 = GraphConv(hidden_size, num_classes)
+
+    def forward(self, g, inputs):
+        h = self.conv1(g, inputs)
+        h = torch.relu(h)
+        h = self.conv2(g, h)
+        return h
+
+net = GCN(5, 5, 2)
+
+inputs = embed.weight
+labeled_nodes = torch.tensor([0, 33])  # only the instructor and the president nodes are labeled
+labels = torch.tensor([0, 1])  # their labels are different
+
+optimizer = torch.optim.Adam(itertools.chain(net.parameters(), embed.parameters()), lr=0.01)
+all_logits = []
+for epoch in range(50):
+    logits = net(G, inputs)
+    # we save the logits for visualization later
+    all_logits.append(logits.detach())
+    logp = F.log_softmax(logits, 1)
+    # we only compute loss for labeled nodes
+    loss = F.nll_loss(logp[labeled_nodes], labels)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    print('Epoch %d | Loss: %.4f' % (epoch, loss.item()))
+
 """
